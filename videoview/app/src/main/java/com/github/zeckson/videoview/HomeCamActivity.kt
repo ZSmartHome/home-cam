d6cb64d9b674363fa854dfbd6bd9cf167a6624f8
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-12-08 21:08:29 +0300

    update android move to kotlin

---
+++ videoview/app/src/main/java/com/github/zeckson/videoview/HomeCamActivity.kt
@@ -0,0 +1,12 @@
+package com.github.zeckson.videoview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeCamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_cam)
+    }
+}
