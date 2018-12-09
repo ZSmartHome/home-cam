bbcc802b209a2a8e25402e83bbe845dfda8b1893
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-12-09 20:24:24 +0300

    add resources setup manifest

---
+++ player/app/src/main/java/com/github/zeckson/rtspvideoplayer/VideoActivity.java
@@ -0,0 +1,6 @@
+package com.github.zeckson.rtspvideoplayer;
+
+import android.app.Activity;
+
+public class VideoActivity extends Activity {
+}
---
+++ player/app/src/main/java/com/github/zeckson/rtspvideoplayer/VrVideoActivity.java
@@ -0,0 +1,6 @@
+package com.github.zeckson.rtspvideoplayer;
+
+import android.app.Activity;
+
+public class VrVideoActivity extends Activity {
+}
